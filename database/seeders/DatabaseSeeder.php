437365308b5e256c5012afd74cb423fef8183245
Bahtiar <zam@kitalulus.com>
2021-04-18 16:57:49 +0700

    add: more seed product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,5 +33,24 @@ class DatabaseSeeder extends Seeder
             'photo' => 'https://images.tokopedia.net/img/cache/900/product-1/2021/3/16/2668215/2668215_4d83f050-b0cf-44e2-bae5-1a906e392a24.jpeg',
             'created_by' => $user->name,
         ]);
+
+        Products::create([
+            'name'  => 'FMC Speed Supply Men Tshirt 120321 - l',
+            'price' => 189500,
+            'description' => '- 24s cotton material
+- Long sleeve
+- Stripe Series
+- Logo on the chest
+- Hot cut at the bottom
+- Model height is 180cm
+- Care label cotton
+
+SIZE (CM) | S | M | L | XL |
+CIRCLE OF THE AGENCY | 101 | 104 | 107 | 110 |
+AGENCY LENGTH | 73 | 75 | 77 | 77 |
+HAND LENGTH | 65 | 66 | 67 | 67 |',
+            'photo' => 'https://images.tokopedia.net/img/cache/900/product-1/2021/4/15/2668215/2668215_32e1dd90-c198-4843-8a50-50a8df3a7a3f.jpeg',
+            'created_by' => $user->name,
+        ]);
     }
 }
