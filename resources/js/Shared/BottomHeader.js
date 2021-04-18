7196170b8e839b8ab35b305575b4329f855002a2
Bahtiar <zam@kitalulus.com>
2021-04-19 01:18:43 +0700

    rm: nav profile

--- resources/js/Shared/BottomHeader.js
+++ resources/js/Shared/BottomHeader.js
@@ -23,14 +23,7 @@ export default () => {
         </div>
         <div className={menuOpened ? '' : 'hidden'}>
           <div className="absolute top-0 right-0 left-auto z-20 py-2 mt-8 text-sm whitespace-nowrap bg-white rounded shadow-xl">
-            <InertiaLink
-              href="#"
-              // href={route('users.edit', auth.user.id)}
-              className="block px-6 py-2 hover:bg-indigo-600 hover:text-white"
-              onClick={() => setMenuOpened(false)}
-            >
-              My Profile
-            </InertiaLink>
+
 
             <InertiaLink
               as="button"
