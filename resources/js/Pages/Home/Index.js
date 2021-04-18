300bf824325575c698490f7d7e56d6cb48aba684
Bahtiar <zam@kitalulus.com>
2021-04-18 22:41:07 +0700

    update: image to full

--- resources/js/Pages/Home/Index.js
+++ resources/js/Pages/Home/Index.js
@@ -7,23 +7,29 @@ const Index = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-6 gap-3">
       {products?.data?.map((item, idx) => (
-        <div key={idx} className="md:p-8 p-2 bg-white cursor-pointer">
-          <img
-            className="rounded-lg w-full"
-            src={item.photo}
-          />
-          <p className="text-indigo-500 font-semibold text-base mt-2">{new Intl.NumberFormat(['ban', 'id']).format(item.price)}</p>
-          <h1
-            className="font-semibold text-gray-900 leading-none text-xl mt-1 capitalize truncate"
+        <InertiaLink
+          key={idx}
+          href={route('product.detail', item?.slug ?? item.id)}
           >
-            {item.name}
-          </h1>
-          <div className="max-w-full">
-            <p className="text-base font-medium tracking-wide text-gray-600 mt-1 whitespace-pre-wrap truncate">
-              {item.description.replace(/\\n/g,'\n').substring(0,50)}
-            </p>
+          <div className="h-full rounded-md bg-white cursor-pointer">
+            <img
+              className="w-full"
+              src={item.photo}
+            />
+            <div className="max-w-full p-3">
+            <p className="text-indigo-500 font-semibold text-base mt-2">{new Intl.NumberFormat(['ban', 'id']).format(item.price)}</p>
+            <h1
+              className="font-semibold text-gray-900 leading-none text-xl mt-1 capitalize truncate"
+            >
+              {item.name}
+            </h1>
+
+              <p className="text-base font-medium tracking-wide text-gray-600 mt-1 whitespace-pre-wrap truncate">
+                {item.description.replace(/\\n/g,'\n').substring(0,50)}
+              </p>
+            </div>
           </div>
-        </div>
+        </InertiaLink>
       ))??[]}
 
     </div>
