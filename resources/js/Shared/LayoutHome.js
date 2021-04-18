85c675fe0e2f43156473020d190e2a82970962ad
Bahtiar <zam@kitalulus.com>
2021-04-18 11:58:03 +0700

    remove: module not used

--- resources/js/Shared/LayoutHome.js
+++ resources/js/Shared/LayoutHome.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import MainMenu from '@/Shared/MainMenu';
-import FlashMessages from '@/Shared/FlashMessages';
 import TopHeader from '@/Shared/TopHeaderHome';
-import BottomHeaderHome from '@/Shared/BottomHeaderHome';
 
 export default function LayoutHome({ title, description ="Opakkuu Shop", children }) {
   return (
