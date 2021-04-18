aa3e37904c462f4d16922b8e5fa5b6524e6a3d9f
Bahtiar <zam@kitalulus.com>
2021-04-19 01:18:25 +0700

    rm: unused

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,6 @@ Route::post('logout')->name('logout')->uses('Auth\LoginController@logout');
 Route::prefix('panel')->group(function () {
     Route::get('/')->name('dashboard')->uses('DashboardController')->middleware('auth');
 
-    // Users @Todo change manange user
-//    Route::get('users')->name('users')->uses('UsersController@index')->middleware('remember', 'auth');
-//    Route::get('users/create')->name('users.create')->uses('UsersController@create')->middleware('auth');
-//    Route::post('users')->name('users.store')->uses('UsersController@store')->middleware('auth');
-//    Route::get('users/{user}/edit')->name('users.edit')->uses('UsersController@edit')->middleware('auth');
-//    Route::put('users/{user}')->name('users.update')->uses('UsersController@update')->middleware('auth');
-//    Route::delete('users/{user}')->name('users.destroy')->uses('UsersController@destroy')->middleware('auth');
-//    Route::put('users/{user}/restore')->name('users.restore')->uses('UsersController@restore')->middleware('auth');
-
     // Images
     Route::get('/img/{path}', 'ImagesController@show')->where('path', '.*');
 
