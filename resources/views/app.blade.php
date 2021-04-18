be691de404d574d5ba3a782dc8714acfb8995b2a
Bahtiar <zam@kitalulus.com>
2021-04-18 16:55:59 +0700

    add: tag google manager

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,11 +8,21 @@
     <script src="{{ mix('js/manifest.js') }}" defer></script>
     <script src="{{ mix('js/vendor.js') }}" defer></script>
     <script src="{{ mix('js/app.js') }}" defer></script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WT8G7PT');</script>
+    <!-- End Google Tag Manager -->
     @routes
 </head>
 <body class="font-sans antialiased leading-none text-gray-800">
 
 @inertia
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WT8G7PT"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) --
 </body>
 </html>
